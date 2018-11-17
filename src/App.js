279c66fbea3689d39b63d7d41ea05a63368765e9
Wanderson Leandro de Oliveira <wleandro.oliveira@gmail.com>
2018-11-16 22:37:54 -0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,59 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
+import ListContacts from './ListContacts'
+import * as ContactsAPI from './utils/ContactsAPI'
+import CreateContact from './CreateContact'
 
 class App extends Component {
+  state = {
+    screen: 'list',
+    contacts: []
+    
+  }
+
+  componentDidMount() {
+    ContactsAPI.getAll().then((contacts)=> {
+      this.setState({ contacts })
+    })
+  }
+  removeContact = contact => {
+    this.setState( state => ({
+      contacts: this.state.contacts.filter((c) => c.id !== contact.id)
+
+    }));
+    ContactsAPI.remove(contact)
+  };
+
+  createContact(contact) {
+    ContactsAPI.create(contact).then(contact => {
+      this.setState(state => ({
+        contacts: state.contacts.concat([ contact ])
+      }))
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Route exact path='/' render={() => (
+          <ListContacts
+            onDeleteContact={this.removeContact}
+            contacts={this.state.contacts}
+            onNavigate = {() => {
+              this.setState({ screen: 'create'})
+            }}
+          />
+        )}/>
+        <Route path='/create' render={({ history }) => (
+          <CreateContact
+          onCreateContact={(contact) => {
+            this.createContact(contact)
+            history.push('/')
+          }}
+          />
+        )}/>
       </div>
-    );
+    )
   }
 }
 
---
+++ src/CreateContact.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import serializeForm from 'form-serialize'
+import ImageInput from './utils/ImageInput'
+
+ class CreateContact extends Component {
+    handleSubmit = (e) => {
+        e.preventDefault()
+        const values = serializeForm(e.target, { hash: true })
+        if (this.props.onCreateContact)
+        this.props.onCreateContact(values)
+    }
+  render() {
+    return (
+    <div>
+        <form onSubmit={this.handleSubmit} className='create-contact-form'>
+            <ImageInput
+                className='create-contact-avatar-input'
+                name='avatarURL'
+                maxHeight={64}
+            />
+            <div className='create-contact-details'>
+                <input type='text' name='name' placeholder='Name'/>
+                <input type='text' name='email' placeholder='Email'/>
+                <button>Add Contact</button>
+            </div>
+        </form>
+   </div>
+   )
+ }
+}
+export default CreateContact
